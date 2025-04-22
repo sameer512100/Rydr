325c3e365819af1b40b90ce7dbd147a250418fed
Sameer Sayyed <sayyedsameer2106@gmail.com>
2025-04-23 02:08:43 +0530

    skbj

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    
-})
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength:[3, 'First name must be at least 3 characters long'],
+        },
+        lastname:{
+            type: String,
+            required: true,
+            minlength:[3, 'Last name must be at least 3 characters long'],
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5, 'Email must be at least 5 characters long'],
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    sockedId:{
+        type:String,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
