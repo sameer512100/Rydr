49a946fdd22c0bd18e0326ca0f2b3c37053df380
Sameer Sayyed <sayyedsameer2106@gmail.com>
2025-04-23 01:59:17 +0530

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const app = require('./App');
+const app = require('./app');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
 
 const server =  http.createServer(app);
 
