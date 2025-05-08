daa72fcefee2e882c8db128146c404f678c36d93
Sameer Sayyed <sayyedsameer2106@gmail.com>
2025-05-08 19:11:45 +0530

    user authentication done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -3,9 +3,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports.authUser= async (req,res,next)=>{
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     if(!token){
         res.status(401).json({message:'Unauthorized'});
+    }
+
+    const isBlack = await userModel.findOne({token:token});
+
+    if(isBlack){
+        res.status(401).json({message:"unauthorized"});
     }
     try {
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
 }
 
